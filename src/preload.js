738a653efcf804531c13a875241cb57b1db8f0af
GTAManRCRX <gaaladrian1@gmail.com>
2026-02-15 15:36:22 +0100

    Preload file auto-generates from now on, so became unnecessary

--- src/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron/renderer");
-
-contextBridge.exposeInMainWorld("appWindow", {
-	goHome: () => ipcRenderer.send("go-home"),
-	openDiscord: () => ipcRenderer.send("open-discord"),
-	openFAQ: () => ipcRenderer.send("open-faq"),
-	openGitHub: () => ipcRenderer.send("open-github"),
-	openDataFolder: () => ipcRenderer.send("open-data-folder"),
-});
